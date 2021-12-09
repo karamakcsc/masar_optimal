7f9ef96c68c252c10061d2c8f322d6047f5fab5f
karamakcsc <karamakcsc@hotmail.com>
2021-12-09 22:42:48 +0000

    Gaith Item

--- masar_optimal/custom/item/item.js
+++ masar_optimal/custom/item/item.js
@@ -1,3 +1,57 @@
+frappe.ui.form.on('Item', {
+onload (frm) {},
+refresh: function(frm){
+  if(user=="ghaith@optimal-motor.com"){
+       cur_frm.dashboard.hide() //Hide DashBoard
+       frm.toggle_display("reorder_section", false);
+       frm.toggle_display("inventory_section", false);
+       frm.toggle_display("barcodes", false);
+       frm.toggle_display("section_break_11", false);
+       frm.toggle_display("item_locations_section", false);
+       frm.toggle_display("unit_of_measure_conversion", false);
+       frm.toggle_display("serial_nos_and_batches", false);
+       frm.toggle_display("variants_section", false);
+       frm.toggle_display("defaults", false);
+       frm.toggle_display("purchase_details", false);
+       frm.toggle_display("supplier_details", false);
+       frm.toggle_display("foreign_trade_details", false);
+       frm.toggle_display("sales_details", false);
+       frm.toggle_display("deferred_revenue", false);
+       frm.toggle_display("deferred_expense_section", false);
+       frm.toggle_display("customer_details", false);
+       frm.toggle_display("item_tax_section_break", false);
+       frm.toggle_display("inspection_criteria", false);
+       frm.toggle_display("manufacturing", false);
+       frm.toggle_display("hub_publishing_sb", false);
+       frm.toggle_display("is_item_from_hub", false);
+       frm.toggle_display("include_item_in_manufacturing", false);
+       
+// Hide Links in Connections Section
+// setTimeout(() => {
+//     $("[data-doctype='BOM']").hide();
+//     $("[data-doctype='Product Bundle']").hide()
+//     $("[data-doctype='Item Price']").hide()
+//     $("[data-doctype='Pricing Rule']").hide()
+//     $("[data-doctype='Quotation']").hide()
+//     $("[data-doctype='Sales Order']").hide()
+//     $("[data-doctype='Delivery Note']").hide()
+//     $("[data-doctype='Sales Invoice']").hide()
+//     $("[data-doctype='Serial No']").hide()
+//     $("[data-doctype='Batch']").hide()
+//     $("[data-doctype='Production Plan']").hide()
+//     $("[data-doctype='Work Order']").hide()
+//     $("[data-doctype='Item Manufacturer']").hide()
+//     $("[data-doctype='Material Request']").hide()
+//     $("[data-doctype='Supplier Quotation']").hide()
+//     $("[data-doctype='Request for Quotation']").hide()
+//     $("[data-doctype='Purchase Order']").hide()
+//     $("[data-doctype='Purchase Receipt']").hide()
+//     $("[data-doctype='Purchase Invoice']").hide()
+//     $("[data-doctype='Stock Entry']").hide()
+//         }, 10);
+}
+}
+});
 
 
 frappe.ui.form.on("Item", {
