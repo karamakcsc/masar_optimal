9bc410d7347284598ea345fd299c5ea8b722f3a0
karamakcsc <karamakcsc@hotmail.com>
2021-12-22 01:57:13 +0000

    mix payment

--- masar_optimal/hooks.py
+++ masar_optimal/hooks.py
@@ -210,7 +210,10 @@ fixtures = [
         "Journal Entry Account-accounting_description",
         "Landed Cost Item-percentage",
         "Landed Cost Voucher-distribute_charges_based_percent",
-        "Payment Entry-ref_number"
+        "Payment Entry-ref_number",
+        "Payment Entry-cheque_amount",
+        "Payment Entry-cash_amount",
+        "Payment Entry-column_break_32"
             ]
         ]
     ]}
